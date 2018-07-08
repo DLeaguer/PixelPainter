bd73158739b0afe505a75e8ffa535a1dac75e99b
DLeaguer <immostlikely@hotmail.com>
2018-07-08 13:12:39 -1000

    fixed erase button, added clear button, cleaned up

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,35 +1,50 @@
 console.log('sanity check');
 
 var ppDiv = document.getElementById('pixelPainter');
-var can = document.createElement('div');
-can.id = 'canvas';
-ppDiv.appendChild(can);
+var canvas = document.createElement('div');
+canvas.id = 'canvas';
+ppDiv.appendChild(canvas);
 
-var allCells = document.createElement('div');
-allCells.id = 'allCells';
-can.appendChild(allCells);
+
+    //      -- leftside color chart --
+var leftSide = document.createElement('div');
+leftSide.id = 'leftSide';
+canvas.appendChild(leftSide);
 
 var divChart = document.createElement('div');
 divChart.id = 'divChart';
-allCells.appendChild(divChart);
+leftSide.appendChild(divChart);
 
-for (var i = 0; i < 72; i++) {
-    var chart = document.createElement('div');
-    chart.id = 'chart';
-    divChart.appendChild(chart);
+for (var i = 0; i < 66; i++) {
+    var chartCells = document.createElement('div');
+    chartCells.className = 'chartCells';
+    divChart.appendChild(chartCells);
 }
 
+
+    //      -- leftside erase button --
 var erase = document.createElement('div');
 erase.id = 'erase';
+erase.className = 'button';
 erase.innerHTML = 'erase';
-divChart.appendChild(erase);
+leftSide.appendChild(erase);
 
-var divCell = document.createElement('div');
-divCell.id = 'divCell';
-allCells.appendChild(divCell);
 
-for (var i = 0; i < 357; i++) {
+    //      -- leftside clear button --
+var clear = document.createElement('div');
+clear.id = 'clear';
+clear.className = 'button';
+clear.innerHTML = 'clear';
+leftSide.appendChild(clear);
+
+
+    //      -- rightside canvas --
+var rightSide = document.createElement('div');
+rightSide.id = 'rightSide';
+canvas.appendChild(rightSide);
+
+for (var i = 0; i < 810; i++) {
     var cell = document.createElement('div');
-    cell.id = 'cells';
-    divCell.appendChild(cell);
+    cell.className = 'cells';
+    rightSide.appendChild(cell);
 }
